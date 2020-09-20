8e342ed93f0bf55de1ed2db0de75d6d674b41f29
Manny Lou <mavkio1@gmail.com>
2020-09-20 02:55:14 -0400

    Added create department function

--- index.js
+++ index.js
@@ -13,36 +13,30 @@ const connection = mysql.createConnection({
 });
 
 
-
-
-
-
-
-
 //Add Employee Function
-function addEmployee(){
+function addEmployee() {
   inquirer.prompt([
     {
-      type:'input',
-      message:`Employee's First Name?`,
-      name:'firstName',
+      type: 'input',
+      message: `Employee's First Name?`,
+      name: 'firstName',
     },
     {
-      type:'input',
-      message:`Employee's Last Name?`,
-      name:'lastName',
+      type: 'input',
+      message: `Employee's Last Name?`,
+      name: 'lastName',
     },
     {
-      type:'list',
-      message:`What will this Employee's Role be?`,
-      choice:['Test','Test','Test'],
-      name:'role',
+      type: 'list',
+      message: `What will this Employee's Role be?`,
+      choice: ['Test', 'Test', 'Test'],
+      name: 'role',
     },
     {
-      type:'input',
-      message:`What is this Employee's department?`,
-      choice: ['HR','IT','Legal','Executive','CS','Development','Security'],
-      name:'department',
+      type: 'input',
+      message: `What is this Employee's department?`,
+      choice: ['HR', 'IT', 'Legal', 'Executive', 'CS', 'Development', 'Security'],
+      name: 'department',
     },
   ]).then((response) => {
     connection.query('INSERT INTO employee(first_name, last_name,)')
@@ -50,14 +44,24 @@ function addEmployee(){
 }
 
 
-
-
-
-
-
-
-
-
+//Create Department
+function createDepartment() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: `Department Name?`,
+      name: 'newDepartment',
+    }
+  ]).then(response => {
+    connection.query(`INSERT INTO department (name) VALUE ('${response.newDepartment}')`, (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      console.table(results);
+      startProgram();
+    })
+  })
+}
 
 
 // Start Program Function
@@ -80,41 +84,42 @@ function startProgram() {
   ]
   inquirer.prompt([{
     type: 'list',
-    message:'What would you like to do?',
+    message: 'What would you like to do?',
     choices: options,
     name: 'choice'
   }]).then(response => {
-    if(response.choice === options[0]){
+    if (response.choice === options[0]) {
       viewEmployees();
-    } else if (response.choice === options [1]){
+    } else if (response.choice === options [1]) {
       addEmployee();
-    } else if (response.choice === options[2]){
+    } else if (response.choice === options[2]) {
       removeEmployee();
-    } else if (response.choice === options[3]){
+    } else if (response.choice === options[3]) {
       updateEmpRole();
-    } else if (response.choice === options[4]){
+    } else if (response.choice === options[4]) {
       updateEmpManager();
-    } else if (response.choice === options[5]){
+    } else if (response.choice === options[5]) {
       newRole();
-    } else if (response.choice === options[6]){
+    } else if (response.choice === options[6]) {
       viewRoles();
-    } else if (response.choice === options[7]){
+    } else if (response.choice === options[7]) {
       removeRole();
-    } else if (response.choice === options[8]){
+    } else if (response.choice === options[8]) {
       createDepartment();
-    } else if (response.choice === options[9]){
+    } else if (response.choice === options[9]) {
       viewDepartments();
-    } else if (response.choice === options[10]){
+    } else if (response.choice === options[10]) {
       removeDepartment();
-    } else if (response.choice === options[11]){
+    } else if (response.choice === options[11]) {
       viewEmpByManager();
-    } else if (response.choice === options[12]){
+    } else if (response.choice === options[12]) {
       viewBudget();
-    } else if (response.choice === options[13]){
+    } else if (response.choice === options[13]) {
       connection.end();
     }
   })
 }
+
 //Program connection to DB
 connection.connect(function (err) {
   if (err) throw  err;
