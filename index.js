9dfeb0bb913dcf039cd8bfe9b691cf26ce909cfe
Manny Lou <mavkio1@gmail.com>
2020-09-20 02:01:53 -0400

    Updated Start Program with more Options

--- index.js
+++ index.js
@@ -19,7 +19,35 @@ const connection = mysql.createConnection({
 
 
 
-
+//Add Employee Function
+function addEmployee(){
+  inquirer.prompt([
+    {
+      type:'input',
+      message:`Employee's First Name?`,
+      name:'firstName',
+    },
+    {
+      type:'input',
+      message:`Employee's Last Name?`,
+      name:'lastName',
+    },
+    {
+      type:'list',
+      message:`What will this Employee's Role be?`,
+      choice:['Test','Test','Test'],
+      name:'role',
+    },
+    {
+      type:'input',
+      message:`What is this Employee's department?`,
+      choice: ['HR','IT','Legal','Executive','CS','Development','Security'],
+      name:'department',
+    },
+  ]).then((response) => {
+    connection.query('INSERT INTO employee(first_name, last_name,)')
+  })
+}
 
 
 
@@ -35,10 +63,20 @@ const connection = mysql.createConnection({
 // Start Program Function
 function startProgram() {
   const options = [
-    'View Current Employees',
-    'Add New Employee',
-    'Delete Current Employee',
-    'Exit',
+    'View Current Employees', //0
+    'Add New Employee', //1
+    'Delete Current Employee', //2
+    'Update Employee Role', //3
+    'Update Employee Manager',//4
+    'Create a New Role', //5
+    'View Current Roles', //6
+    'Delete Role', //7
+    'Create Department', //8
+    'View Department', //9
+    'Delete Department', //10
+    'View Employee by Manager', //11
+    'View budget by Departments', //12
+    'Exit', //13
   ]
   inquirer.prompt([{
     type: 'list',
@@ -53,6 +91,26 @@ function startProgram() {
     } else if (response.choice === options[2]){
       removeEmployee();
     } else if (response.choice === options[3]){
+      updateEmpRole();
+    } else if (response.choice === options[4]){
+      updateEmpManager();
+    } else if (response.choice === options[5]){
+      newRole();
+    } else if (response.choice === options[6]){
+      viewRoles();
+    } else if (response.choice === options[7]){
+      removeRole();
+    } else if (response.choice === options[8]){
+      createDepartment();
+    } else if (response.choice === options[9]){
+      viewDepartments();
+    } else if (response.choice === options[10]){
+      removeDepartment();
+    } else if (response.choice === options[11]){
+      viewEmpByManager();
+    } else if (response.choice === options[12]){
+      viewBudget();
+    } else if (response.choice === options[13]){
       connection.end();
     }
   })
